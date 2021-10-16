2cd96c7ea21ebcfd0117148c5ef5eb8e4c495ba5
Nandemonaiya233 <91420589+Nandemonaiya233@users.noreply.github.com>
2021-10-16 20:11:47 +0800

    Add files via upload

---
+++ 代码实现/huiwenzifupanduan.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindromebyRecurrence(string str) {
+
+    int strLen = str.length();
+
+    if (strLen <= 1) {
+        return true;
+    }
+
+    if (str[0] == str[strLen - 1]) {
+        return isPalindromebyRecurrence(str.substr(1, strLen - 2));
+    }
+
+    return false;
+
+}
+
+bool isPalindromeCharbyRecurrence(const char* str, int strLen) {
+
+    if (strLen <= 1) {
+        return true;
+    }
+
+
+    if (str[0] == str[strLen - 1]) {
+        return isPalindromeCharbyRecurrence(str + 1, strLen - 2);
+    }
+
+    return false;
+
+}
+
+bool isPalindromebyStep(string str) {
+
+    int strLen = str.length();
+    bool isPalindrome = true;
+
+    if (strLen <= 1) {
+        return true;
+    }
+
+    for (int i = 0; i < strLen/2; i++) {
+        if (str[i] != str[strLen - i - 1]){
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    return isPalindrome;
+
+
+}
+
+bool isPalindromeCharbyStep(const char* str, int strLen) {
+
+    bool isPalindrome = true;
+
+    if (strLen <= 1) {
+        return true;
+    }
+
+
+    for (int i = 0; i < strLen/2; i++) {
+        if (str[i] != str[strLen - i - 1]){
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    return isPalindrome;
+
+
+}
+
+int main()
+{
+    string isPalindrome = " is a palindrome";
+    string notPalindrome = " is not a palindrome";
+
+    const char* str = "";
+    if (isPalindromeCharbyRecurrence(str, 0)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "a";
+    if (isPalindromeCharbyRecurrence(str, 1)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "ab";
+    if (isPalindromeCharbyRecurrence(str, 2)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "aba";
+    if (isPalindromeCharbyRecurrence(str, 3)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "abba";
+    if (isPalindromeCharbyRecurrence(str, 4)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "cccbccc";
+    if (isPalindromeCharbyRecurrence(str, 7)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "aaaaa";
+    if (isPalindromeCharbyRecurrence(str, 5)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    cout << "========================================" << endl;
+    str = "";
+    if (isPalindromeCharbyStep(str, 0)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "a";
+    if (isPalindromeCharbyStep(str, 1)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "ab";
+    if (isPalindromeCharbyStep(str, 2)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "aba";
+    if (isPalindromebyRecurrence(str)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "abba";
+    if (isPalindromeCharbyStep(str, 4)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "cccbccc";
+    if (isPalindromeCharbyStep(str, 7)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+
+    str = "aaaaa";
+    if (isPalindromeCharbyStep(str, 5)) {
+        cout << str << isPalindrome << endl;
+    }
+    else {
+        cout << str << notPalindrome << endl;
+    }
+}
---
+++ 代码实现/jiafafenjie.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+void printOutput(int number, int* decompositions,int decompositionCount) {
+    if (NULL == decompositions || decompositionCount < 1) {
+        cout << "Please check you implementation." << endl;
+    }
+    cout << number << "=";
+    int i = 0;
+    for (; i < decompositionCount - 1; i++) {
+        cout << decompositions[i] << "+";
+    }
+    cout << decompositions[i] << endl;
+}
+
+void decompseNumberRepeated(int number,  int curDecomposeNumber, 
+                              int* decompositions,int decompositionCount) {
+    if (curDecomposeNumber == 0) {
+        // Get one decompostion
+        printOutput(number, decompositions, decompositionCount);
+    }
+    else {
+        for (int i = 1; i <= curDecomposeNumber; i++)
+        {
+            decompositions[decompositionCount] = i;
+            decompseNumberRepeated(number, curDecomposeNumber - i, 
+                                     decompositions, decompositionCount + 1);
+        }
+        
+    }           
+}
+
+void decompseNumberNotRepeated(int number,  int curDecomposeNumber, 
+                              int* decompositions,int decompositionCount) {
+    if (curDecomposeNumber == 0) {
+        // Get one decompostion
+        printOutput(number, decompositions, decompositionCount);
+    }
+    else {
+        int startIndex = decompositionCount > 0 ? decompositions[decompositionCount - 1] : 1;
+        for (int i = startIndex; i <= curDecomposeNumber; i++)
+        {
+            decompositions[decompositionCount] = i;
+            decompseNumberNotRepeated(number, curDecomposeNumber - i, 
+                                     decompositions, decompositionCount + 1);
+        }
+        
+    }           
+}
+
+int main()
+{
+    int n,m;
+	cin>>n>>m;
+	int number = n;
+	int* decompostions = new int[n];
+	if(m==1)
+		decompseNumberRepeated(number, n, decompostions, 0);
+	else if(m==2)
+		decompseNumberNotRepeated(number, n, decompostions, 0);
+    8
+    delete decompostions;
+	return 0;
+}
